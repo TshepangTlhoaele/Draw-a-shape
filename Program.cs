db525e420f0d8a0c02e7d6044d3466a511d7ec6c
Lenovo-User <Lenovo-User@DESKTOP-KGOC38Q>
2024-01-24 03:06:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_a_shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine("_____ ");
+
+            //to pause the console else it closes on its own!
+            Console.ReadLine();
+        }
+    }
+}
